591350f417e2e74ccb3677f774c895de4909bae4
typhone-99 <typhoooone.99@gmail.com>
2025-02-11 18:42:36 +0900

    Updatew

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -244,9 +244,7 @@ export default function RoomPage() {
           className="px-4 py-2 border border-zinc-200 rounded-lg w-full placeholder:text-zinc-400 outline-none duration-200 focus-visible:ring-2 ring-offset-2"
           placeholder="Your Name"
         />
-      </div>
-
-      <div className="my-8 flex md:hidden items-center border border-zinc-200 rounded-lg p-2 shadow-sm sticky top-4 bg-white z-50">
+              <div className="mt-4 flex md:hidden items-center border border-zinc-200 rounded-lg p-2 shadow-sm sticky top-4 bg-white z-50">
         <input
           type="text"
           value={message}
@@ -260,6 +258,7 @@ export default function RoomPage() {
         >
           <FaPaperPlane />
         </button>
+      </div>
       </div>
 
       {votes.length > 0 && (
